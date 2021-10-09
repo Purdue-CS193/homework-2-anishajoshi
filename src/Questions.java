f7baf486ddef4433dbfbebaa31be32a37f49487c
Anisha Joshi <47920708+anishajoshi@users.noreply.github.com>
2021-10-09 09:35:57 +0800

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -118,14 +118,14 @@ public class Questions {
         // look for the index of target in input
         int low = 0;
         int high = input.length - 1;
-        while (low < high) {
-            int mid = (low + high) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             if (input[mid] == target) { // middle element is the target. Success!!!
                 return mid;
             } else if (input[mid] > target) { // middle element is greater than the target
-                low = mid + 1;
-            } else { // middle element is smaller than the target
                 high = mid - 1;
+            } else { // middle element is smaller than the target
+                low = mid + 1;
             }
         }
         return -1; // element is not found
