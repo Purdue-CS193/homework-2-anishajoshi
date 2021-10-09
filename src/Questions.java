03802fc33a412ef130e517214c3096e404a4219e
Anisha Joshi <47920708+anishajoshi@users.noreply.github.com>
2021-10-09 09:18:09 +0800

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -1,19 +1,19 @@
 /*
-* CS 193 Homework 3
-*
-* TODO: READ THESE INSTRUCTIONS!
-*
-* The comments in the methods are for clarification. No tricks or riddles.
-*
-* The overall algorithm should not change. Don't focus on improving the runtime. 
-*
-* Debug in whichever way you feel is best.
-*
-* IT WILL BE AN AUTOMATIC 0, IF YOU MAKE CHANGES TO TESTCASES.JAVA
-*
-* Bonus: Understand these methods! They are helpful in interviews :)
-* 
-*/
+ * CS 193 Homework 3
+ *
+ * TODO: READ THESE INSTRUCTIONS!
+ *
+ * The comments in the methods are for clarification. No tricks or riddles.
+ *
+ * The overall algorithm should not change. Don't focus on improving the runtime.
+ *
+ * Debug in whichever way you feel is best.
+ *
+ * IT WILL BE AN AUTOMATIC 0, IF YOU MAKE CHANGES TO TESTCASES.JAVA
+ *
+ * Bonus: Understand these methods! They are helpful in interviews :)
+ *
+ */
 
 import java.util.*;
 
@@ -22,8 +22,9 @@ public class Questions {
     // Task 1
     public static int findMax(int[] input) {
         // find the max in the input array
-        int max = Integer.MAX_VALUE;
-        for (int i = 0; i <= input.length; i++) {
+        // Hello, I was told to make a change to the Questions.java file, so I did :)))
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < input.length; i++) {
             if (input[i] > max) {
                 max = input[i];
             }
@@ -34,9 +35,9 @@ public class Questions {
     // Task 2
     public static int findMin(int[] input) {
         // find the smallest element in the array
-        int min = Integer.MIN_VALUE;
-        for (int i = 0; i <= input.length; i++) {
-            if (input[i] > min) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] < min) {
                 min = input[i];
             }
         }
@@ -47,7 +48,7 @@ public class Questions {
     public static int findSum(int[] input) {
         // find the sum of all the elements in the array
         int sum = 0;
-        for (int i = 1; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++) {
             sum = sum + input[i];
         }
         return sum;
@@ -57,10 +58,12 @@ public class Questions {
     public static int findAverage(int[] input) {
         // find the average of the input
         int sum = 0;
-        for (int i = 1; i < input.length; i++) {
-            sum = input[i] - sum;
+        for (int i = 0; i < input.length; i++) {
+            sum = input[i] + sum;
+            System.out.println(sum);
+            System.out.println(input.length);
         }
-        int average = sum / (input.length - 1);
+        int average = sum / (input.length);
         return average;
     }
 
@@ -79,19 +82,19 @@ public class Questions {
         ArrayList<String> answer = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 1) {
-                answer.add("fizz");
-            } else if (i % 5 == 1) {
-                answer.add("buzz");
-            } else if (i % 15 == 1) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
                 answer.add("fizzbuzz");
+            } else if (i % 3 == 0) {
+                answer.add("fizz");
+            } else if (i % 5 == 0) {
+                answer.add("buzz");
             } else {
                 answer.add(Integer.toString(i));
             }
         }
         return answer;
     }
-    
+
     // Task 6
     public static int reverseNumber(int input) {
         // reverse the number
@@ -105,9 +108,9 @@ public class Questions {
         }
         return answer;
     }
-    
+
     //EXTRA CREDIT BELOW HERE
-    
+
     // Task 7
     // Look for a specific element in sorted array
     // keep in mind for this algorithm to work, array HAS to be sorted
@@ -127,7 +130,7 @@ public class Questions {
         }
         return -1; // element is not found
     }
-    
+
     // Task 8
     public static int countDuplicates(String input) {
         // Count the number of letters that are duplicated
@@ -195,6 +198,6 @@ public class Questions {
     public static void main(String[] args) {
         // Example given below
         // Run reverseNumber with your own input
-        System.out.println(reverseNumber(54321)); 
+        System.out.println(reverseNumber(54321));
     }
 }
